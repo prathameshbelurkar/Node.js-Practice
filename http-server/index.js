097eefe4d7525caf01230b75adf69877a54f6f9a
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2022-05-16 15:36:40 +0530

    Initial Commit

---
+++ http-server/index.js
@@ -0,0 +1,17 @@
+const http = require("http");
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-type": "application/json" });
+  res.end(
+    JSON.stringify({
+      id: 1,
+      name: "Sir Isaac Newton",
+    })
+  );
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});

031dc73c0fe1130d79f8c4dcdf909c4b7cc5708b
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2022-05-16 17:27:49 +0530

    index.js: Updated

--- http-server/index.js
+++ http-server/index.js
@@ -1,19 +1,40 @@
+// HTTP Module
 const http = require("http");
+const { stringify } = require("querystring");
 
+// Setting PORT
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/friends") {
+// Creating Server
+const server = http.createServer();
+
+// Data
+const friends = [
+  {
+    id: 0,
+    name: "Nikola Tesla",
+  },
+  {
+    id: 1,
+    name: "Sir Isaac Newton",
+  },
+];
+
+// Handling Request
+server.on("request", (req, res) => {
+  const items = req.url.split("/");
+
+  if (items[1] === "friends") {
     res.statusCode = 200;
     res.setHeader("Content-Type", "application/json");
     res.writeHead(200, { "Content-type": "application/json" });
-    res.end(
-      JSON.stringify({
-        id: 1,
-        name: "Sir Isaac Newton",
-      })
-    );
-  } else if (req.url === "/messages") {
+    if (items.length === 3) {
+      const friendIndex = Number(items[2]);
+      res.end(JSON.stringify(friends[friendIndex]));
+    } else {
+      res.end(JSON.stringify(friends));
+    }
+  } else if (items[1] === "messages") {
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
     res.write("<body>");
@@ -28,6 +49,7 @@ const server = http.createServer((req, res) => {
   }
 });
 
+// Listen Server
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 });

31802e56101cebdeb8d6de561857d3e7eb0c6651
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2022-05-16 15:57:58 +0530

    Add README

--- http-server/index.js
+++ http-server/index.js
@@ -3,13 +3,29 @@ const http = require("http");
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-type": "application/json" });
-  res.end(
-    JSON.stringify({
-      id: 1,
-      name: "Sir Isaac Newton",
-    })
-  );
+  if (req.url === "/friends") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(
+      JSON.stringify({
+        id: 1,
+        name: "Sir Isaac Newton",
+      })
+    );
+  } else if (req.url === "/messages") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<body>");
+    res.write("<ul>");
+    res.write("<li>Hello Isaac!</li>");
+    res.write("<li>What are your thoughts on astronomy?</li>");
+    res.write("</body>");
+    res.end();
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
 });
 
 server.listen(PORT, () => {
